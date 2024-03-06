c269f2e9873205afb148d252efb547517e409a69
Polin-Dybovskaya <90262722+Polin-Dybovskaya@users.noreply.github.com>
2024-03-06 10:01:25 +0300

    ...

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,16 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
-def f():
-    return render_template('2.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/error')
-def r():
+def error():
     return render_template('error.html'),400
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("error.html"), 404
+
 
 
