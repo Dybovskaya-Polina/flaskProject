2b763a4ab79e284cda3fa524286a135f57c3a760
FicusPlol <rambler124413@gmail.com>
2024-03-12 17:48:00 +0300

    form changed

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,7 @@ class ContactForm(FlaskForm):
     name = StringField("Nickname: ",
                        validators=[Length(min=2, max=30, message="The name must be between 2 and 30 characters long")])
     email = StringField("Email: ", validators=[Email("Invalid email")])
-    psw = PasswordField("Password: ", validators=[DataRequired(), Length(min=4, max=30,
+    psw = PasswordField("Password: ", validators=[DataRequired(), Length(min=2, max=30,
                                                                          message="The password must be between 4 and 30 characters long")])
     psw2 = PasswordField("Confirm password: ",
                          validators=[DataRequired(), EqualTo('psw', message="Passwords don't match")])
